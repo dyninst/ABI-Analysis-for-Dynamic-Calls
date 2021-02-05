b394feaa507bf37e207cdb4a7d17a7677ad44bf4
Tim Haines <thaines.astro@gmail.com>
2021-02-05 17:14:29 -0600

    Add an additional test function to dlopen_test
    This is to see if Dyninst can find calls to functions declared in the same CU.

--- example/dlopen_test.cpp
+++ example/dlopen_test.cpp
@@ -1,12 +1,18 @@
 #include <dlfcn.h>
 #include <cstdio>
 
+int foo() {
+  return printf("Hello");
+}
+
 int main() {
 	void *handle = dlopen("mylib.so", RTLD_LAZY);
 	if(!handle) {
 		fprintf(stderr, "dlopen failed: %s\n", dlerror());
 		return -1;
 	}
+
+	foo();
 	
 	void *func = dlsym(handle, "hello");
 	if(func) {
